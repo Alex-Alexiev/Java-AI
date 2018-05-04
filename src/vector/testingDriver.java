b4c211dcea8397462c7485e53da56b200dc04925
Alex Alexiev <aalexiev@bayviewglen.ca>
2018-05-04 00:18:05 -0400

    My first single neuron network made from scratch.

---
+++ src/vector/testingDriver.java
@@ -0,0 +1,14 @@
+package vector;
+
+public class testingDriver {
+
+	public static void main(String[] args) {
+		
+		Vector v1 = new Vector(new double[] {0,1,2,3,4});
+		Vector v2 = new Vector(new double[] {1,3,6,3,2});
+		System.out.println(v1.add(v2));
+		
+		
+	}
+
+}
